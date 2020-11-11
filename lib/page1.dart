b946d913d39a6876f160cf3620d3aff215003023
Ricardo Villarroel <50605189+Cianny148@users.noreply.github.com>
2020-11-11 18:13:47 -0300

    Ppquete de dibujo

--- lib/page1.dart
+++ lib/page1.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:iwg_proyect/page2.dart';
+import 'package:iwg_proyect/page3.dart';
 
 class FirstRoute extends StatelessWidget {
   @override
@@ -27,23 +28,32 @@ class FirstRoute extends StatelessWidget {
 //###################### Widgets en columna #######################
 
 //Imagen avatar redonda-------------------------------------------------------
-            CircleAvatar(
-              radius: 90.0,
-              backgroundImage: AssetImage('icons/New_Drawing.png'),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 50, 0, 200),
+                child: CircleAvatar(
+                  radius: 90.0,
+                  backgroundImage: AssetImage('icons/New_Drawing.png'),
+              )
             ),
 
 //Boton de prueba------------------------------------------------------------------
-            ElevatedButton(
+//            ElevatedButton(
 //Funcion del boton------------------
+//              onPressed: () {
+//                Navigator.push(context, new MaterialPageRoute(builder: (context) => ThirdRoute()));
+//              }, child: Text('Pagina 3'),
+//            ),
+
+//Boton-------------------------------------------------------------------
+            FlatButton(
+//Funcion del boton------------
               onPressed: () {
                 Navigator.push(context, new MaterialPageRoute(builder: (context) => SecondRoute()));
-              }, child: Text('Pagina 2'),
-            ),
-
-//Tarjeta normal------------------------------------------------------------------
-            Card(
+              },
+//Child del boton---------------
+              child: Card(
               margin: EdgeInsets.symmetric(
-                horizontal: 30.0,
+                horizontal: 0.0,
                 vertical:30.0,
               ),
               child: Text('A Dibujar !!',
@@ -55,21 +65,23 @@ class FirstRoute extends StatelessWidget {
               ),
               color: Colors.indigo,
               shadowColor: Colors.orange,
+            ), 
             ),
+//-------------------------------------------------------------------
 
 //Boton-------------------------------------------------------------------
             FlatButton(
 //Funcion del boton------------
               onPressed: () {
-                Navigator.push(context, new MaterialPageRoute(builder: (context) => SecondRoute()));
+                Navigator.push(context, new MaterialPageRoute(builder: (context) => ThirdRoute()));
               },
 //Child del boton---------------
               child: Card(
               margin: EdgeInsets.symmetric(
                 horizontal: 30.0,
-                vertical:30.0,
+                vertical:5.0,
               ),
-              child: Text('A Dibujar !!',
+              child: Text('Opciones',
                 style: TextStyle(
                   color: Colors.white,
                   fontFamily: 'IndieFlower',
